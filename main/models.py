36a408eaddd21358857a6b6d59c3df325891fe12
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-01-26 19:37:16 +0200

    feat models: add phone_number validator

--- main/models.py
+++ main/models.py
@@ -8,8 +8,14 @@ from django.contrib.contenttypes import fields, models as contrib_models
 class AppUserManager(BaseUserManager):
     @staticmethod
     def normalize_phone_number(phone_number):
-        # TODO: IMPLEMENT PHONE_NUMBER VALIDATION
-        return phone_number
+        # Remove any non-digit characters from the phone number
+        digits_only = ''.join(filter(str.isdigit, phone_number))
+
+        if len(digits_only) != 12:
+            raise ValueError(
+                'Invalid phone number format. Valid phone number must contain country code (3 digits) and other 9 digits. 12 in total')
+
+        return digits_only
 
     def create_user(self, first_name, last_name, username, phone_number, email, password=None, **kwargs):
         if not first_name:
