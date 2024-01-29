7e19db78095573e1c4fdd563872e0635f6ea95c4
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-01-30 00:32:48 +0200

    feat:  created views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth import get_user_model
+from .models import Post
+from .serializers import PostSerializer
+from .mixins import LikeModelMixin
 
-# Create your views here.
+User = get_user_model()
+
+
+class PostViewSet(ModelViewSet, LikeModelMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
