5aacc7b944ff963174a7460de847e445465a35b6
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-01-30 00:54:09 +0200

    feat: implement urls

--- SocialNetwork/urls.py
+++ SocialNetwork/urls.py
@@ -5,7 +5,7 @@ URL configuration for SocialNetwork project.
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
-from main.views import PostViewSet
+from main.views import PostViewSet, LikesAnalytics, UserActivity
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
@@ -19,6 +19,9 @@ urlpatterns = [
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/v1/docs/',
          SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    path('api/analytics/', LikesAnalytics.as_view(), name='likes-analytics'),
+    path('api/user-activity/<str:username>/',
+         UserActivity.as_view(), name='user-activity'),
 ]
 
 urlpatterns += router.urls
